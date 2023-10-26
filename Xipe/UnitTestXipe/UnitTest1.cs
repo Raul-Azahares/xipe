44bcaa0153d92428c6dcc777e3ea82bc26c41090
Raul <raul.azahares@centrohub.co>
2023-10-27 01:21:26 +0900

    add two simple test

--- Xipe/UnitTestXipe/UnitTest1.cs
+++ Xipe/UnitTestXipe/UnitTest1.cs
@@ -1,14 +1,39 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Xipe_Conexion;
 
 namespace UnitTestXipe
 {
     [TestClass]
-    public class UnitTest1
+    public class DatabaseTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestOpenConexion()
         {
+            // Arrange
+            IDatabase database = new MySQLDatabase();
+
+            // Act
+            database.OpenConexion();
+            bool conexionOpen = true; 
+            Assert.IsTrue(conexionOpen, "the conexion open fine");
+        }
+        [TestMethod]
+        public void TestWriteConexion()
+        {
+
+            IDatabase database = new MySQLDatabase();
+
+            database.OpenConexion();
+
+            string message = "Data of MySQL";
+            database.WriteConexion(message);
+
+
+            string messageWrite = "Data of MySQL"; 
+            string messageInDatabase = database.Read(); 
+
+            Assert.AreEqual(messageWrite, messageInDatabase, "the message is equal");
         }
     }
 }
