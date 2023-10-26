5ba6112183965cb7fd15f307d4efce0e8d7045bf
Raul <raul.azahares@centrohub.co>
2023-10-27 00:33:42 +0900

    add methods of interface IDatabasse

--- Xipe/Xipe_Conexion/Program.cs
+++ Xipe/Xipe_Conexion/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace Xipe_Conexion
 {
+
+    public interface IDatabase {
+        void OpenConexion();
+        void WriteConexion(string message);
+        string Read();
+        void CloseConexion();
+
+    
+    }
+    public class MySQLDatabase : IDatabase
+    {
+        public void OpenConexion()
+        {
+            Console.WriteLine("Connection to MySQL open");
+        }
+
+        public void WriteConexion(string message)
+        {
+            Console.WriteLine($"Connection in MySQL: {message}");
+        }
+
+        public string Read()
+        {
+            Console.WriteLine("Reading of MySQL");
+            return "Data of MySQL";
+        }
+
+        public void CloseConexion()
+        {
+            Console.WriteLine("Connection to MySQL closed");
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
